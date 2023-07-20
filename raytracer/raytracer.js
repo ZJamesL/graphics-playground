8e2a9cf96c9f076c8192c0bb5d0219fcaf11fe69
Devolafriend <zjlopez@arizona.edu>
2023-07-20 12:52:09 -0700

    raytracing

---
+++ raytracer/raytracer.js
@@ -0,0 +1,12 @@
+
+
+// canvas variables 
+
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d");
+
+context.fillStyle = "rgb(200, 0, 0)";
+context.fillRect(249, 20, 50, 50);
+
+context.fillStyle = 'rgba(0, 0, 200, 0.5)';
+context.fillRect(30, 30, 50, 50);
